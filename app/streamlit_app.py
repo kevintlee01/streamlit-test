4cacc440bdd9bdbf367a80bb965324a3f676593e
Kevin Lee <42420798+kevintlee01@users.noreply.github.com>
2023-10-30 18:28:13 -0700

    Update streamlit_app.py

--- app/streamlit_app.py
+++ app/streamlit_app.py
@@ -5,7 +5,8 @@ import matplotlib.pyplot as plt
 st.title('Earthquake Data Explorer')
 st.text('This is a web app to explore earthquake data.')
 
-upload_file = st.file_uploader('Upload your file here')
+#upload_file = st.file_uploader('Upload your file here')
+upload_file = st.file_uploader('all_day.csv')
 
 if upload_file:
   df = pd.read_csv(upload_file)
@@ -15,7 +16,7 @@ if upload_file:
   st.write(df.head())
 
   fig, ax = plt.subplot(1, 1)
-  ax.scatter(x = df['Depth of Earthquake'], y = df['Magnitude of Earthquake'])
+  ax.scatter(x = df['depth'], y = df['mag'])
   ax.set_xlabel('Depth of Earthquake')
   ax.set_ylabel('Magnitude of Earthquake')
 
