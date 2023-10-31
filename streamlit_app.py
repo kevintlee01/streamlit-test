eb1be8117bc420f9513dd274f5358ab82f31f36b
Kevin Lee <42420798+kevintlee01@users.noreply.github.com>
2023-10-30 18:10:41 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,6 @@
+import matplotlib.pyplot as plt
 import streamlit as st
 import pandas as pd
-import matplotlib.pyplot as plt
 
 st.title('Earthquake Data Explorer')
 st.text('This is a web app to explore earthquake data.')
